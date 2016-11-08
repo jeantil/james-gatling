ad615debdb663ec0c30ef94dad7c0a2fc16cb352
Antoine Duprat <aduprat@linagora.com>
2016-11-08 13:57:45 +0100

    Add hasNoRecent check

--- src/main/scala-2.11/org/apache/james/gatling/imap/check/ImapCheckSupport.scala
+++ src/main/scala-2.11/org/apache/james/gatling/imap/check/ImapCheckSupport.scala
@@ -2,4 +2,5 @@ package org.apache.james.gatling.imap.check
 
 trait ImapCheckSupport {
   def ok = ImapSimpleCheck(_.lastOption.exists(_.isOK))
+  def hasNoRecent = ImapSimpleCheck(_.exists(_.toString.contains("0 RECENT")))
 }
