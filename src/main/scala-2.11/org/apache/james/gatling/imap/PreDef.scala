245c9251b3c7fe90966e5517da0e3ac44be465ed
Jean Helou <jean.helou@gmail.com>
2016-11-08 17:32:25 +0100

    Remove compiler warning about implicit conversions

--- src/main/scala-2.11/org/apache/james/gatling/imap/PreDef.scala
+++ src/main/scala-2.11/org/apache/james/gatling/imap/PreDef.scala
@@ -1,13 +1,15 @@
 package org.apache.james.gatling.imap
 
+import scala.language.implicitConversions
+
 import org.apache.james.gatling.imap.action.ImapActionBuilder
 import org.apache.james.gatling.imap.check.ImapCheckSupport
 import org.apache.james.gatling.imap.protocol.{ImapProtocol, ImapProtocolBuilder}
 
 object PreDef extends ImapCheckSupport {
 
-  def imap = ImapProtocolBuilder.default
+  def imap: ImapProtocolBuilder = ImapProtocolBuilder.default
   implicit def imapProtocolBuilder2ImapProtocol(builder: ImapProtocolBuilder): ImapProtocol = builder.build()
 
-  def imap(requestName: String) = new ImapActionBuilder(requestName)
+  def imap(requestName: String): ImapActionBuilder = new ImapActionBuilder(requestName)
 }
