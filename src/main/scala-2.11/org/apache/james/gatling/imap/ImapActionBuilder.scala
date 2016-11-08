25eb5f4772e36ed8b43987adb795a68c89813832
Jean Helou <jean.helou@gmail.com>
2016-11-08 15:19:16 +0100

    extract common build method for Imap command action builders

--- src/main/scala-2.11/org/apache/james/gatling/imap/ImapActionBuilder.scala
+++ src/main/scala-2.11/org/apache/james/gatling/imap/ImapActionBuilder.scala
@@ -1,48 +1,66 @@
 package org.apache.james.gatling.imap
 
+import scala.collection.immutable.Seq
+
+import akka.actor.Props
 import io.gatling.core.action.builder.ActionBuilder
 import io.gatling.core.action.{Action, ExitableActorDelegatingAction}
 import io.gatling.core.session.Expression
+import io.gatling.core.stats.StatsEngine
 import io.gatling.core.structure.ScenarioContext
 import io.gatling.core.util.NameGen
 import org.apache.james.gatling.imap.action.{ConnectAction, LoginAction, SelectAction}
 import org.apache.james.gatling.imap.check.ImapCheck
 import org.apache.james.gatling.imap.protocol.{ImapComponents, ImapProtocol}
-import scala.collection.immutable.Seq
 
 class ImapActionBuilder(requestName: String) {
-  def login(user:Expression[String], password:Expression[String]): ImapLoginActionBuilder = {
-    ImapLoginActionBuilder(requestName, user,password)
+  def login(user: Expression[String], password: Expression[String]): ImapLoginActionBuilder = {
+    ImapLoginActionBuilder(requestName, user, password)
   }
-  def select(mailbox:Expression[String]): ImapSelectActionBuilder = {
+
+  def select(mailbox: Expression[String]): ImapSelectActionBuilder = {
     ImapSelectActionBuilder(requestName, mailbox, Seq.empty)
   }
+
   def connect(): ImapConnectActionBuilder = {
     ImapConnectActionBuilder(requestName)
   }
 }
 
-case class ImapLoginActionBuilder(requestName: String, username:Expression[String], password:Expression[String]) extends ActionBuilder with NameGen {
+abstract class ImapCommandActionBuilder extends ActionBuilder with NameGen {
+  def props(statsEngine: StatsEngine, next: Action, components: ImapComponents): Props
+
+  def requestName: String
+
+  def actionName: String
+
   override def build(ctx: ScenarioContext, next: Action): Action = {
     val components: ImapComponents = ctx.protocolComponentsRegistry.components(ImapProtocol.ImapProtocolKey)
-    val actionActor = ctx.system.actorOf(LoginAction.props(components.protocol, components.sessions, requestName, ctx.coreComponents.statsEngine, next, username, password), "login-action")
+    val actionActor = ctx.system.actorOf(props(ctx.coreComponents.statsEngine, next, components), actionName)
     new ExitableActorDelegatingAction(genName(requestName), ctx.coreComponents.statsEngine, next, actionActor)
   }
+
 }
 
-case class ImapSelectActionBuilder(requestName: String, mailbox:Expression[String], private val checks: Seq[ImapCheck]) extends ActionBuilder with NameGen {
-  def check(checks: ImapCheck*) = copy(checks=this.checks ++ checks)
-  override def build(ctx: ScenarioContext, next: Action): Action = {
-    val components: ImapComponents = ctx.protocolComponentsRegistry.components(ImapProtocol.ImapProtocolKey)
-    val actionActor = ctx.system.actorOf(SelectAction.props(components.protocol, components.sessions, requestName, ctx.coreComponents.statsEngine, next, checks, mailbox), "select-action")
-    new ExitableActorDelegatingAction(genName(requestName), ctx.coreComponents.statsEngine, next, actionActor)
-  }
+case class ImapLoginActionBuilder(requestName: String, username: Expression[String], password: Expression[String]) extends ImapCommandActionBuilder {
+  override def props(statsEngine: StatsEngine, next: Action, components: ImapComponents) =
+    LoginAction.props(components.protocol, components.sessions, requestName, statsEngine, next, username, password)
+
+  override val actionName = "login-action"
 }
 
-case class ImapConnectActionBuilder(requestName: String) extends ActionBuilder with NameGen {
-  override def build(ctx: ScenarioContext, next: Action): Action = {
-    val components: ImapComponents = ctx.protocolComponentsRegistry.components(ImapProtocol.ImapProtocolKey)
-    val actionActor = ctx.system.actorOf(ConnectAction.props(components.protocol, components.sessions, requestName, ctx.coreComponents.statsEngine, next),"connect-action")
-    new ExitableActorDelegatingAction(genName(requestName), ctx.coreComponents.statsEngine, next, actionActor)
-  }
+case class ImapSelectActionBuilder(requestName: String, mailbox: Expression[String], private val checks: Seq[ImapCheck]) extends ImapCommandActionBuilder {
+  def check(checks: ImapCheck*) = copy(checks = this.checks ++ checks)
+
+  override def props(statsEngine: StatsEngine, next: Action, components: ImapComponents) =
+    SelectAction.props(components.protocol, components.sessions, requestName, statsEngine, next, checks, mailbox)
+
+  override val actionName = "select-action"
+}
+
+case class ImapConnectActionBuilder(requestName: String) extends ImapCommandActionBuilder {
+  override def props(statsEngine: StatsEngine, next: Action, components: ImapComponents): Props =
+    ConnectAction.props(components.protocol, components.sessions, requestName, statsEngine, next)
+
+  override val actionName: String = "connect-action"
 }
